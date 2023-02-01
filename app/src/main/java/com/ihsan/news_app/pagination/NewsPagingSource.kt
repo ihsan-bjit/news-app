8da7faae1b39541bbfa1a3bbb8980d147dcb78e1
ihsan <ihsan.alaqib@bjitacademy.com>
2023-02-01 17:02:19 +0600

    new branch paging

---
+++ app/src/main/java/com/ihsan/news_app/pagination/NewsPagingSource.kt
@@ -0,0 +1,44 @@
+package com.ihsan.news_app.pagination
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.ihsan.news_app.model.Article
+import com.ihsan.news_app.repository.NewsRepository
+
+private const val TMDB_STARTING_PAGE_INDEX = 1
+class NewsPagingSource(private val repository: NewsRepository): PagingSource<Int, Article>() {
+    override fun getRefreshKey(state: PagingState<Int, Article>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+        }
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Article> {
+        val pageIndex = params.key ?: TMDB_STARTING_PAGE_INDEX
+        return try {
+            val response = service.getTopRatedMovies(
+                language = "en-US",
+                page = pageIndex
+            )
+            val movies = response.results
+            val nextKey =
+                if (movies.isEmpty()) {
+                    null
+                } else {
+                    // By default, initial load size = 3 * NETWORK PAGE SIZE
+                    // ensure we're not requesting duplicating items at the 2nd request
+                    pageIndex + (params.loadSize / NETWORK_PAGE_SIZE)
+                }
+            LoadResult.Page(
+                data = movies,
+                prevKey = if (pageIndex == TMDB_STARTING_PAGE_INDEX) null else pageIndex,
+                nextKey = nextKey
+            )
+        } catch (exception: IOException) {
+            return LoadResult.Error(exception)
+        } catch (exception: HttpException) {
+            return LoadResult.Error(exception)
+        }
+    }
+}
